4df1bdfb0e89394fa9b9a84aeb26040a4975d259
espositoandrea <esposito_andrea99@hotmail.com>
2020-12-15 19:00:58 +0100

    Fixed error

--- src/Parser/Core.hs
+++ src/Parser/Core.hs
@@ -13,6 +13,15 @@ instance Functor Parser where
                 Nothing            -> Nothing
                 Just (env, v, out) -> Just (env, g v, out))
 
+instance Applicative Parser where
+  -- pure :: a -> Parser a
+  pure v = P(\env input -> Just (env, v, input))
+
+  -- <*> :: Parser (a -> b) -> Parser a -> Parser b
+  pf <*> pa = P(\env input -> case parse pf env input of
+                  Nothing            -> Nothing
+                  Just (env, f, out) -> parse (fmap f pa) env input)
+
 instance Monad Parser where
   -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
   p >>= f = P(\env input -> case parse p env input of
@@ -20,7 +29,7 @@ instance Monad Parser where
                 Just (env, v, out) -> parse (f v) env out)
 
   -- return :: a -> Parser a
-  return v = P(\env input -> Just (env, v, input))
+  return v = pure v
 
 instance Alternative Parser where
   -- empty :: Parser a
